87562e22d5ab126018d598c2b888476877d2bc25
Ashish Upadhyay <upadhyay.ashish246@gmail.com>
2024-10-08 09:03:38 +0530

    Update search_expense.java

--- hexaware/src/test/java/main/search_expense.java
+++ hexaware/src/test/java/main/search_expense.java
@@ -1,38 +1,27 @@
 package main;
-import src.main.*;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
-
 import src.util.*;
 import src.dao.*;
 import src.entity.*;
-
-
-
-import org.junit.jupiter.api.Test;
 class search_expense {
-	private FinanceRepositoryImpl financeRepository;
+	private FinanceRepositoryImpl repo;
     private Connection conn;
 	
 	@Test
-	void testGetAllExpenses_Success() throws SQLException {
-		 conn = DBConnUtil.getConnection("db.properties");
-	        if (conn != null) {
-	            System.out.println("Connection established.");
-	        }
-	    // Arrange
-	    int userId = 3;  // Assuming user with ID 1 exists and has expenses
-	    FinanceRepositoryImpl financeRepository = new FinanceRepositoryImpl(conn);
-
-	    // Act
-	    List<Expense> expenses = financeRepository.getAllExpenses(userId);
-
-	    // Assert
+	void testGetAllExpenses_Success() throws SQLException 
+	{
+	 	conn = DBConnUtil.getConnection("db.properties");
+        if (conn != null)
+        {
+            System.out.println("Connection established.");
+        }
+	    int userId = 3;  
+	    repo = new FinanceRepositoryImpl(conn);
+	    List<Expense> expenses = repo.getAllExpenses(userId);
 	    assertNotNull(expenses, "Error -> The expenses list should not be null");
 	    assertTrue(expenses.size() > 0, "Error -> Expected expenses for the user but got an empty list");
 	}
