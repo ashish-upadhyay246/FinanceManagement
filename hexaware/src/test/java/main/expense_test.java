b7046ac74f1e69f88b7c5b610379be9dfbd6c94b
Ashish Upadhyay <upadhyay.ashish246@gmail.com>
2024-10-08 09:02:46 +0530

    Update expense_test.java

--- hexaware/src/test/java/main/expense_test.java
+++ hexaware/src/test/java/main/expense_test.java
@@ -1,8 +1,5 @@
 package main;
-import src.main.*;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,27 +7,21 @@ import src.util.*;
 import src.dao.*;
 import src.entity.*;
 
-import org.junit.jupiter.api.Test;
-
 class expense_test {
-	  private FinanceRepositoryImpl financeRepository;
-	    private Connection conn;
-
+	  private FinanceRepositoryImpl repo;
+	  private Connection conn;
 
 	@Test
-	void testCreateExpense_Success() throws SQLException {
-		 conn = DBConnUtil.getConnection("db.properties");
-	        if (conn != null) {
-	            System.out.println("Connection established.");
-	        }
-	    // Arrange
+	void testCreateExpense_Success() throws SQLException 
+	{
+		conn = DBConnUtil.getConnection("db.properties");
+        if (conn != null) 
+        {
+            System.out.println("Connection established.");
+        }
 	    Expense testExpense = new Expense(3, 0, 150.0, 2, new java.util.Date(), "Groceries");
-	    FinanceRepositoryImpl financeRepository = new FinanceRepositoryImpl(conn);
-
-	    // Act
-	    boolean result = financeRepository.createExpense(testExpense);
-
-	    // Assert
+	    repo = new FinanceRepositoryImpl(conn);
+	    boolean result = repo.createExpense(testExpense);
 	    assertTrue(result, "Error -> Expense was not created successfully");
 	}
 
