bd21e53c6b01da9d005ab96ff4598d8f565015a5
Ashish Upadhyay <upadhyay.ashish246@gmail.com>
2024-10-08 15:09:27 +0530

    Delete hexaware directory

--- hexaware/src/main/java/src/exception/ExpenseNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package src.exception;
-
-public class ExpenseNotFoundException extends Exception 
-{
-    public ExpenseNotFoundException(String msg) 
-    {
-        super(msg);
-    }
-}
--- hexaware/src/main/java/src/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package src.exception;
-
-public class UserNotFoundException extends Exception 
-{
-    public UserNotFoundException(String msg) 
-    {
-        super(msg);
-    }
-}
